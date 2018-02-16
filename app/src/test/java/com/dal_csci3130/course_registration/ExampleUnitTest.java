2593d174ff922f77375f4f8ed09274b14488e227
kamiunix <sam.c.tur@gmail.com>
2018-02-16 16:53:15 -0400

    init

---
+++ app/src/test/java/com/dal_csci3130/course_registration/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.dal_csci3130.course_registration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
