40d1211d3eccd9c20e163e5c176f6aeb38257fb5
AJ <al909955@dal.ca>
2018-02-22 19:36:05 -0400

    Enabled the string arrays to appear in the two spinners.

--- app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
+++ app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
@@ -8,6 +8,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
+
+/**
+ * Created by AJM-D on 2018-02-22.
+ * This class is used to search for information using filters.
+ *
+ */
+
 public class Main2Activity extends AppCompatActivity {
 
     @Override
@@ -15,10 +22,16 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_view_results);
         Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner3);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.faculty_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.year_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter2);
 
     }
 }
