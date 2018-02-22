e45abdcdc87186843cd21e035f10c5907dae51c0
AJ <al909955@dal.ca>
2018-02-22 19:00:15 -0400

    Dropdown spinner now appears with the string arrays in the filters.xml

--- app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
+++ app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-
+import android.widget.ArrayAdapter;
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_view_results);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.faculty_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
