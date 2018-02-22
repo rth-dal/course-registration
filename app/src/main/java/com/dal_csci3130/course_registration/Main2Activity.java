b831c70f740b5fee14240f6ac70aae5d9c998402
AJ <al909955@dal.ca>
2018-02-22 19:53:44 -0400

    Included another string array into the filters.xml named 'term' for another filter as designed.

--- app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
+++ app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
@@ -21,17 +21,22 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_view_results);
+        //Creation of the spinners used to display the string array filters
         Spinner spinner = (Spinner) findViewById(R.id.spinner2);
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner3);
-
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner4);
+        //Calls the array to be displayed
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.faculty_array, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.year_array, android.R.layout.simple_spinner_item);
-
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.term_array, android.R.layout.simple_spinner_item);
+        //Sets the adapters view from resource
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Sets the spinner
         spinner.setAdapter(adapter);
         spinner2.setAdapter(adapter2);
+        spinner3.setAdapter(adapter3);
 
     }
 }
