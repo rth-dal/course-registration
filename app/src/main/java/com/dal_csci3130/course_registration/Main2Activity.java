08e9abf237c583a7f5625ec1ee8df90a152058ce
AJ <al909955@dal.ca>
2018-02-22 18:44:08 -0400

    Created launcher activity and set default layout as search.

---
+++ app/src/main/java/com/dal_csci3130/course_registration/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.dal_csci3130.course_registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_view_results);
+    }
+}
