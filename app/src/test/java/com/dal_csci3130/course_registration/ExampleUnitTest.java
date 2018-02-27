390847ca29d2b26e3f9df9e907b0c6fd198bc317
hoyya <padraecr@gmail.com>
2018-02-26 23:44:40 -0400

    Added se

--- app/src/test/java/com/dal_csci3130/course_registration/ExampleUnitTest.java
+++ app/src/test/java/com/dal_csci3130/course_registration/ExampleUnitTest.java
@@ -11,6 +11,10 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
 
+    private String[] faculty_list = {"CSCI", "PHIL", "CHEM", "BIOL", "MATH"};
+    private int[] year_list = {1,2,3,4,6,8};
+    private int[] openspots_list = {0,10,20,50,100};
+
     filtered_search search1 = new filtered_search();
     filtered_search search2 = new filtered_search();
     filtered_search search3 = new filtered_search();
