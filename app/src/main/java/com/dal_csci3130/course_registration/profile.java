ce0a14a6f98df08e9ab49f2e4bf9d7bbbfb23511
Brandon Hussey <brandon.hussey048@gmail.com>
2018-03-07 14:59:44 -0400

    <pair BH RH> begin set up of default filter on profile

--- app/src/main/java/com/dal_csci3130/course_registration/profile.java
+++ app/src/main/java/com/dal_csci3130/course_registration/profile.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class profile extends AppCompatActivity {
+
+    public String Filter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,17 @@ public class profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         Button classSearch = (Button) findViewById(R.id.classSearch);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.faculty_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        Filter1 = spinner.getSelectedItem().toString();
+
+
 
         classSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +38,25 @@ public class profile extends AppCompatActivity {
 
 
                 startActivity(new Intent(profile.this, Main2Activity.class));
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Filter1 = "CSCI";
+                        break;
+                    case 1:
+                        Filter1 = "MATH";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
     }
