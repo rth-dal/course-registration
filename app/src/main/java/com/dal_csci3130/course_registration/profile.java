135c7ed02f69e036f22cbb2c2ba4117fd984833b
Brandon Hussey <brandon.hussey048@gmail.com>
2018-03-07 15:47:17 -0400

    <pair BH RH> minor tweaks to default filter on profile

--- app/src/main/java/com/dal_csci3130/course_registration/profile.java
+++ app/src/main/java/com/dal_csci3130/course_registration/profile.java
@@ -11,7 +11,10 @@ import android.widget.Spinner;
 
 public class profile extends AppCompatActivity {
 
-    public String Filter1;
+    public String Faculty;
+    public String Major;
+    public String Minor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,25 @@ public class profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         Button classSearch = (Button) findViewById(R.id.classSearch);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner facultySpinner = (Spinner) findViewById(R.id.facultyspinner);
+        Spinner majorSpinner = (Spinner) findViewById(R.id.majorspinner);
+        Spinner minorSpinner = (Spinner) findViewById(R.id.minorspinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.faculty_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> facultyAdapter = ArrayAdapter.createFromResource(this, R.array.faculty_array2, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> majorAdapter = ArrayAdapter.createFromResource(this, R.array.major_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> minorAdapter = ArrayAdapter.createFromResource(this, R.array.minor_array, android.R.layout.simple_spinner_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        facultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        majorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(adapter);
+        facultySpinner.setAdapter(facultyAdapter);
+        majorSpinner.setAdapter(majorAdapter);
+        minorSpinner.setAdapter(minorAdapter);
 
-        Filter1 = spinner.getSelectedItem().toString();
-
+        Faculty = facultySpinner.getSelectedItem().toString();
+        Major = majorSpinner.getSelectedItem().toString();
+        Minor = minorSpinner.getSelectedItem().toString();
 
 
         classSearch.setOnClickListener(new View.OnClickListener() {
@@ -41,15 +53,88 @@ public class profile extends AppCompatActivity {
             }
         });
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        facultySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 switch (i) {
                     case 0:
-                        Filter1 = "CSCI";
+                        Faculty = "";
                         break;
                     case 1:
-                        Filter1 = "MATH";
+                        Faculty = "BCS";
+                        break;
+                    case 2:
+                        Faculty = "BACS";
+                        break;
+                    case 3:
+                        Faculty = "BSC";
+                        break;
+                    case 4:
+                        Faculty = "BCOMM";
+                        break;
+                    case 5:
+                        Faculty = "BA";
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        majorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Major = "";
+                        break;
+                    case 1:
+                        Major = "Computer Science";
+                        break;
+                    case 2:
+                        Major = "Mathematics";
+                        break;
+                    case 3:
+                        Major = "Management";
+                        break;
+                    case 4:
+                        Major = "Biology";
+                        break;
+                    case 5:
+                        Major = "Political Science";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        minorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Minor = "";
+                        break;
+                    case 1:
+                        Minor = "Computer Science";
+                        break;
+                    case 2:
+                        Minor = "Math";
+                        break;
+                    case 3:
+                        Minor = "Management";
+                        break;
+                    case 4:
+                        Minor = "Biology";
+                        break;
+                    case 5:
+                        Minor = "Political Science";
                         break;
                 }
             }
